a91b6af9d8749b498e71ac383638bdbff4b22d6e
priyanka251996 <priyankaradhakrishnan96@gmail.com>
2016-01-09 23:48:30 +0530

    Created by Coding Ground Client

---
+++ New Project/HelloWorld.java
@@ -0,0 +1,494 @@
+class HelloWorld
+{
+int A,B,C,D,E,F;
+int a,b,c,d,e,f;
+int A1,B1,C1,D1,E1;
+int a1,b1,c1,d1,e1,f1;
+int co,pds,evs,pqt,dbms;int ans1,ans2;
+int os,flc,algo,dm,sw,wt;
+ static String name="pinky";   
+      HelloWorld()
+{    
+}
+
+HelloWorld(int co,int pds,int evs,int pqt,int dbms)
+{
+    this.co=co;this.pds=pds;this.evs=evs;
+    this.pqt=pqt;this.dbms=dbms;
+    
+}
+HelloWorld(int os1,int flc1,int algo1,int dm1,int sw1,int wt1)
+    {
+        os=os1;
+        flc=flc1;algo=algo1;dm=dm1;wt=wt1;
+        sw=sw1;
+    }
+   
+   void calcu(int n,int m,int h,int l,int r)
+   {
+       A=n;
+       B=m;
+       C=h;
+       D=l;
+       E=r;
+          }
+          
+          
+          
+          void calcu1(int aqq,int bqq,int cqq,int eqq,int fqq,int gqq)
+   {
+       a=aqq;
+       b=bqq;
+       c=cqq;       d=eqq;
+       e=fqq;
+    f=gqq;
+        
+        
+          }
+          
+          
+   
+    
+  void display()
+    
+    {
+        System.out.println(co+" "+pqt+" "+evs+" "+pds+" "+dbms);
+        
+         System.out.println(os+" "+dm+" "+wt+" "+sw+" "+algo+" "+flc);
+        
+            }
+            
+            void calci()
+            {
+                if(co>90)
+                {
+                    A1=10;
+                }
+                else if(co>80)
+                {
+                    A1=9;
+                }
+                else if(co>70)
+                {
+                    A1=8;
+                }
+                else if(co>60)
+                {
+                    A1=7;
+                }
+                else if(co>50)
+                {
+                    A1=6;
+                }
+                else if(co>40)
+                {
+                    A1=5;
+                }
+                else
+                {
+                    A1=4;
+                }
+                
+                     if(pds>90)
+                {
+                    B1=10;
+                }
+                else if(pds>80)
+                {
+                    B1=9;
+                }
+                else if(pds>70)
+                {
+                    B1=8;
+                }
+                else if(pds>60)
+                {
+                    B1=7;
+                }
+                else if(pds>50)
+                {
+                    B1=6;
+                }
+                else if(pds>40)
+                {
+                    B1=5;
+                }
+                else
+                {
+                    B1=4;
+                }
+                
+    
+                 if(dbms>90)
+                {
+                    C1=10;
+                }
+                else if(dbms>80)
+                {
+                    C1=9;
+                }
+                else if(dbms>70)
+                {
+                    C1=8;
+                }
+                else if(dbms>60)
+                {
+                    C1=7;
+                }
+                else if(dbms>50)
+                {
+                    C1=6;
+                }
+                else if(dbms>40)
+                {
+                    C1=5;
+                }
+                else
+                {
+                    C1=4;
+                }
+                
+                     if(pqt>90)
+                {
+                    D1=10;
+                }
+                else if(pqt>80)
+                {
+                    D1=9;
+                }
+                else if(pqt>70)
+                {
+                    D1=8;
+                }
+                else if(pqt>60)
+                {
+                    D1=7;
+                }
+                else if(pqt>50)
+                {
+                    D1=6;
+                }
+                else if(pqt>40)
+                {
+                    D1=5;
+                }
+                else
+                {
+                    D1=4;
+                }
+                
+    
+                
+                 if(evs>90)
+                {
+                    E1=10;
+                }
+                else if(evs>80)
+                {
+                    E1=9;
+                }
+                else if(evs>70)
+                {
+                    E1=8;
+                }
+                else if(evs>60)
+                {
+                    E1=7;
+                }
+                else if(evs>50)
+                {
+                    E1=6;
+                }
+                else if(evs>40)
+                {
+                    E1=5;
+                }
+                else
+                {
+                    E1=4;
+                }
+                
+            }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                void calci1()
+            {
+                if(wt>90)
+                {
+                    a1=10;
+                }
+                else if(wt>80)
+                {
+                    a1=9;
+                }
+                else if(wt>70)
+                {
+                    a1=8;
+                }
+                else if(wt>60)
+                {
+                    a1=7;
+                }
+                else if(wt>50)
+                {
+                    a1=6;
+                }
+                else if(wt>40)
+                {
+                    a1=5;
+                }
+                else
+                {
+                    a1=4;
+                }
+                
+                     if(sw>90)
+                {
+                    b1=10;
+                }
+                else if(sw>80)
+                {
+                    b1=9;
+                }
+                else if(sw>70)
+                {
+                    b1=8;
+                }
+                else if(sw>60)
+                {
+                    b1=7;
+                }
+                else if(sw>50)
+                {
+                    b1=6;
+                }
+                else if(sw>40)
+                {
+                    b1=5;
+                }
+                else
+                {
+                    b1=4;
+                }
+                
+    
+                 if(dm>90)
+                {
+                    c1=10;
+                }
+                else if(dm>80)
+                {
+                    c1=9;
+                }
+                else if(dm>70)
+                {
+                    c1=8;
+                }
+                else if(dm>60)
+                {
+                    c1=7;
+                }
+                else if(dm>50)
+                {
+                    c1=6;
+                }
+                else if(dm>40)
+                {
+                    c1=5;
+                }
+                else
+                {
+                    c1=4;
+                }
+                
+                     if(flc>90)
+                {
+                    d1=10;
+                }
+                else if(flc>80)
+                {
+                    d1=9;
+                }
+                else if(flc>70)
+                {
+                    d1=8;
+                }
+                else if(flc>60)
+                {
+                    d1=7;
+                }
+                else if(flc>50)
+                {
+                    d1=6;
+                }
+                else if(flc>40)
+                {
+                    d1=5;
+                }
+                else
+                {
+                    d1=4;
+                }
+                
+    
+                
+                 if(algo>90)
+                {
+                    e1=10;
+                }
+                else if(algo>80)
+                {
+                    e1=9;
+                }
+                else if(algo>70)
+                {
+                    e1=8;
+                }
+                else if(algo>60)
+                {
+                    e1=7;
+                }
+                else if(algo>50)
+                {
+                    e1=6;
+                }
+                else if(algo>40)
+                {
+                    e1=5;
+                }
+                else
+                {
+                    e1=4;
+                }
+                
+                if(os>90)
+                {
+                    f1=10;
+                }
+                else if(os>80)
+                {
+                    f1=9;
+                }
+                else if(os>70)
+                {
+                    f1=8;
+                }
+                else if(os>60)
+                {
+                    f1=7;
+                }
+                else if(os>50)
+                {
+                    f1=6;
+                }
+                else if(os>40)
+                {
+                    f1=5;
+                }
+                else
+                {
+                    f1=4;
+                }            }
+                
+    
+    void gpa()
+    {
+        int s1,s2,s3,s4,s5;
+    int total;
+        int l;
+        s1=A1*A;
+        s2=B1*B;
+        s3=C1*C;
+        s4=D1*D;
+        s5=E1*E;
+        total=s1+s2+s3+s4+s5;
+    f=A+B+C+D+E;
+    
+    ans1=total/f;
+    
+    System.out.println("gpa1"+" "+ans1);
+    
+    
+    }
+    
+    
+    
+    void gpa1()
+    {
+        int s1,s2,s3,s4,s5;
+int s6;    int total;
+        int l;
+        s1=a1*a;
+        s2=b1*b;
+        s3=c1*c;
+        s4=d1*d;
+        s5=e1*e;
+        s6=f1*f;
+        total=s1+s2+s3+s4+s5;
+    f=a+b+c+d+e+f;
+    ans2=total/f;
+    
+    
+    
+    System.out.println("gpa"+" "+ans2);
+    }
+    
+    void cgpa()
+    {
+        int h;
+    int h2;
+    h=ans1+ans2;
+    h2=h/2;
+        System.out.println("cgpa"+" "+h2);
+        
+    }
+    
+    
+    
+    
+    
+    
+    public static void main(String args[])
+    {
+        HelloWorld bbc=new HelloWorld(77,88,68,94,62);
+        HelloWorld bbc1=new HelloWorld(80,77,68,88,67,55);
+      HelloWorld bn=new HelloWorld();
+      
+        
+        bbc.calcu(5,4,4,3,2);
+        bbc.calci();
+bbc1.calcu1(5,4,4,3,2,3);
+bbc1.calci1();
+        bbc.gpa();
+    bbc1.gpa1(); 
+    bbc1.cgpa();
+        
+    }
+    
+                
+                
+                
+                     
+                     
+                     
+            
+            
+    
+            
+            
+            
+    
+    
+}
