3925921ed7930dca401fd58a445b778d9e50f026
priyanka251996 <priyankaradhakrishnan96@gmail.com>
2016-01-21 23:23:16 +0530

    Created by Coding Ground Client

---
+++ New Project/Aaa.java
@@ -0,0 +1,26 @@
+interface Aaa
+{
+    
+   public  void disp();
+}
+  
+  
+class Bbb implements Aaa
+{
+ public void disp(){   
+    System.out.println("sdsdfsd");
+ }
+ public static void main(String args[])
+ {
+     
+     Bbb n=new Bbb();
+     n.disp();
+     
+ }
+}
+     
+
+    
+    
+    
+    
